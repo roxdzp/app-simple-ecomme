1a174f6c0cfd07a4377e397f11079e0eb385f41d
roxdzp <41691189+roxdzp@users.noreply.github.com>
2024-06-17 19:55:24 -0300

    Update config.php
    Cambio el nombre de la variable de entorno

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', '$_ENV[dbhost]');
+	define('DB_HOST', getenv('DB_ENDPOINT'));
     define('DB_USER', 'admin');
     define('DB_PASSWORD', 'passwordort');
     define('DB_DATABASE', 'db_web');
