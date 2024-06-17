d7ea46773a0663eccb1abdd813df66d73e4e7514
roxdzp <41691189+roxdzp@users.noreply.github.com>
2024-06-17 19:35:43 -0300

    Update config.php
    Cambiando los parámetros

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', 'root');
-    define('DB_DATABASE', 'idukan');
+	define('DB_HOST', '$_ENV[dbhost]');
+    define('DB_USER', 'admin');
+    define('DB_PASSWORD', 'passwordort');
+    define('DB_DATABASE', 'db_web');
 ?>
