a0b1dd511c106705c5ed3ad8bf1e27b2792850ba
Rocio Diaz <rociobdiazp@gmail.com>
2024-06-19 21:41:50 -0300

    Edito el archivo config.php para usar variables

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', getenv('DB_ENDPOINT'));
+	define('DB_HOST', 'db-endpoint');
     define('DB_USER', 'admin');
-    define('DB_PASSWORD', 'passwordort');
+    define('DB_PASSWORD', 'db-password');
     define('DB_DATABASE', 'db_web');
 ?>
